947dd88b2fe0a4a750cb04489b139a9f84149fa9
baileywickham <baileywickham@gmail.com>
2020-12-30 14:33:02 -0800

    add gui

---
+++ gui/tui.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	c "github.com/jroimartin/gocui"
+)
+
+func keybindings(g *c.Gui) error {
+	if err := g.SetKeybinding("main", c.KeyArrowDown, c.ModNone, cursorDown); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("main", c.KeyArrowUp, c.ModNone, cursorUp); err != nil {
+		return err
+	}
+
+	if err := g.SetKeybinding("", c.KeyCtrlC, c.ModNone,
+		func(g *c.Gui, v *c.View) error { return c.ErrQuit }); err != nil {
+		return err
+	}
+
+	if err := g.SetKeybinding("msg", c.KeyEnter, c.ModNone, delMsg); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("main", c.KeyEnter, c.ModNone, getLine); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var PROMPT_VIEW = " Prompt View "
+
+func layout(g *c.Gui) error {
+	//if _, err := CreateLeftHalfView(g, "hi", "2"); err != nil {
+	//	return err
+	//}
+	if _, err := CreateTopRightQuarterView(g, "tr", "3"); err != nil {
+		return err
+	}
+	if _, err := CreateBottomRightQuarterView(g, "br", "4"); err != nil {
+		return err
+	}
+	if _, err := CreateBottomLeftQuarterView(g, "bl", "3"); err != nil {
+		return err
+	}
+	if _, err := CreateTopLeftQuarterView(g, "tl", "4"); err != nil {
+		return err
+	}
+
+	return nil
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("main", 0, 0, maxX-1, maxY-1); err != nil {
+		if err != c.ErrUnknownView {
+			return err
+		}
+		// highlight items
+		v.Frame = true
+		v.Title = " main "
+		v.Highlight = true
+		v.SelBgColor = c.ColorGreen
+		v.SelFgColor = c.ColorBlack
+
+		fmt.Fprintln(v, "Item 1")
+		fmt.Fprintln(v, "Item 2")
+		fmt.Fprintln(v, "Item 3")
+		fmt.Fprint(v, "\rWill be")
+		fmt.Fprint(v, "deleted\rItem 4\nItem 5")
+
+		if _, err := g.SetCurrentView("main"); err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func main() {
+	g, err := c.NewGui(c.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.Cursor = true
+
+	g.SetManagerFunc(layout)
+
+	if err := keybindings(g); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != c.ErrQuit {
+		log.Panicln(err)
+	}
+}
---
+++ gui/ui_helper.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	c "github.com/jroimartin/gocui"
+)
+
+func cursorDown(g *c.Gui, v *c.View) error {
+	if v != nil {
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy+1); err != nil {
+			ox, oy := v.Origin()
+			if err := v.SetOrigin(ox, oy+1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func cursorUp(g *c.Gui, v *c.View) error {
+	if v != nil {
+		ox, oy := v.Origin()
+		cx, cy := v.Cursor()
+		if err := v.SetCursor(cx, cy-1); err != nil && oy > 0 {
+			if err := v.SetOrigin(ox, oy-1); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func delMsg(g *c.Gui, v *c.View) error {
+	if err := g.DeleteView("msg"); err != nil {
+		return err
+	}
+	if _, err := g.SetCurrentView("main"); err != nil {
+		return err
+	}
+	return nil
+}
+func getLine(g *c.Gui, v *c.View) error {
+	var l string
+	var err error
+
+	_, cy := v.Cursor()
+	if l, err = v.Line(cy); err != nil {
+		l = ""
+	}
+
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("msg", maxX/2-30, maxY/2, maxX/2+30, maxY/2+2); err != nil {
+		if err != c.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, l)
+		v.Editable = true
+		if _, err := g.SetCurrentView("msg"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func CreatePromptView(g *c.Gui, title string) error {
+	var PROMPT_VIEW = " Prompt View "
+	tw, th := g.Size()
+	v, err := g.SetView(PROMPT_VIEW, tw/6, (th/2)-1, (tw*5)/6, (th/2)+1)
+	if err != nil && err != c.ErrUnknownView {
+		return err
+	}
+	v.Editable = true
+	v.Title = title
+	g.Cursor = true
+	_, err = g.SetCurrentView(PROMPT_VIEW)
+
+	return err
+}
+
+func CreateLeftHalfView(g *c.Gui, title string) (*c.View, error) {
+	tw, th := g.Size()
+	v, err := g.SetView("lh", 0, 0, (tw/2)-1, th-1)
+	if err != nil && err != c.ErrUnknownView {
+		return nil, err
+	}
+	v.Editable = true
+	v.Title = title
+	g.Cursor = true
+
+	v, err = g.SetCurrentView("lh")
+
+	return v, err
+
+}
+
+func CreateRightHalfView(g *c.Gui, title string) (*c.View, error) {
+	tw, th := g.Size()
+	v, err := g.SetView("rh", (tw/2)+1, 0, tw-1, th-1)
+	if err != nil && err != c.ErrUnknownView {
+		return nil, err
+	}
+	v.Editable = true
+	v.Title = title
+	g.Cursor = true
+
+	v, err = g.SetCurrentView("rh")
+
+	return v, err
+}
+
+func CreateTopRightQuarterView(g *c.Gui, title string) (*c.View, error) {
+	tw, th := g.Size()
+	// x0, y0, x1, y1
+	v, err := g.SetView("tr", (tw/2)+1, 0, tw-1, (th/2)-1)
+	if err != nil && err != c.ErrUnknownView {
+		return nil, err
+	}
+	v.Editable = true
+	v.Title = title
+	g.Cursor = true
+
+	v, err = g.SetCurrentView("tr")
+
+	return v, err
+}
+
+func CreateBottomRightQuarterView(g *c.Gui, title string) (*c.View, error) {
+	tw, th := g.Size()
+	// x0, y0, x1, y1
+	v, err := g.SetView("br", (tw/2)+1, (th/2)+1, tw-1, th-1)
+	if err != nil && err != c.ErrUnknownView {
+		return nil, err
+	}
+	v.Editable = true
+	v.Title = title
+	g.Cursor = true
+
+	v, err = g.SetCurrentView("br")
+
+	return v, err
+}
+
+func CreateTopLeftQuarterView(g *c.Gui, title string) (*c.View, error) {
+	tw, th := g.Size()
+	// x0, y0, x1, y1
+	v, err := g.SetView("tl", 0, 0, (tw/2)-1, (th/2)-1)
+	if err != nil && err != c.ErrUnknownView {
+		return nil, err
+	}
+	v.Editable = true
+	v.Title = title
+	g.Cursor = true
+
+	v, err = g.SetCurrentView("tl")
+
+	return v, err
+}
+
+func CreateBottomLeftQuarterView(g *c.Gui, title string) (*c.View, error) {
+	tw, th := g.Size()
+	// x0, y0, x1, y1
+	v, err := g.SetView("bl", 0, (th/2)+1, (tw/2)-1, th-1)
+	if err != nil && err != c.ErrUnknownView {
+		return nil, err
+	}
+	v.Editable = true
+	v.Title = title
+	g.Cursor = true
+
+	v, err = g.SetCurrentView("bl")
+
+	return v, err
+}
+
+func setTopWindowTitle(g *c.Gui, view_name, title string) {
+	v, err := g.View(view_name)
+	if err != nil {
+		log.Println("Error on setTopWindowTitle", err)
+		return
+	}
+	v.Title = fmt.Sprintf("%v (Ctrl-q to close)", title)
+}
+
+func keybindings(g *c.Gui) error {
+
+}
